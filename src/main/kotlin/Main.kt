4f156f7eb21f8196f0d9ba3e645447e38cc77938
Harshad Nawathe <harshadn@thoughtworks.com>
2022-08-19 16:38:58 +0530

    Use if / else to take advantage of smart cast to non-nullable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
     print("Your name: ")
     val name = readlnOrNull()
-    val nameLength = name?.length ?: 0
-    println("Hello, $name${"!".repeat(nameLength)}")
+    if(name != null) {
+        val nameLength = name.length
+        println("Hello, $name${"!".repeat(nameLength)}")
+    } else {
+        println("Hi")
+    }
+
 }
