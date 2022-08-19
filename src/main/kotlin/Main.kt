c0e81f897e30d6219ed3b5ccf353ee2c16cffde7
Harshad Nawathe <harshadn@thoughtworks.com>
2022-08-19 16:41:54 +0530

    Use function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 fun main(args: Array<String>) {
     print("Your name: ")
     val name = readlnOrNull()
-    val message = if(name != null) {
-        val nameLength = name.length
-        "Hello, $name${"!".repeat(nameLength)}"
-    } else {
-        "Hi"
-    }
+    val message = messageFor(name)
     println(message)
+}
+
+private fun messageFor(name: String?) = if (name != null) {
+    val nameLength = name.length
+    "Hello, $name${"!".repeat(nameLength)}"
+} else {
+    "Hi"
 }
