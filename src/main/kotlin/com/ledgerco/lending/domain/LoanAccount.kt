d509ee6ac1536fd14f655530d37680544836b843
Harshad Nawathe <harshadn@thoughtworks.com>
2022-08-22 18:29:59 +0530

    Include lump-sum payments in the amount paid calculation.

--- src/main/kotlin/com/ledgerco/lending/domain/LoanAccount.kt
+++ src/main/kotlin/com/ledgerco/lending/domain/LoanAccount.kt
@@ -6,8 +6,20 @@ class LoanAccount(
     val loan: Loan
 ) {
     private val payments = ArrayList<Payment>()
+    fun amountPaid(monthNo: Int): Int {
+        val paidThroughEmi = monthNo * loan.emi()
+        val paidThroughLumpSum = payments.filter { it.paidAfterMonthNo < monthNo }
+            .sumOf(Payment::amount)
 
-    fun amountPaid(monthNo: Int) = monthNo * loan.emi()
+//        var paidThroughLumpSum = 0
+//        for (payment in payments) {
+//            if(payment.paidAfterMonthNo < monthNo) {
+//                paidThroughLumpSum += payment.amount
+//            }
+//        }
+
+        return paidThroughEmi + paidThroughLumpSum
+    }
     fun amountRemaining(monthNo: Int) = loan.totalAmount() - amountPaid(monthNo)
     fun balance(monthNo: Int) = Balance(
         bankName = bankName,
