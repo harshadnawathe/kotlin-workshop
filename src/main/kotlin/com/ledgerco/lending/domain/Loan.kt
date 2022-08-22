c02e38cbd7cc8b29d779cbbc24a6ddd8e9528426
Harshad Nawathe <harshadn@thoughtworks.com>
2022-08-22 17:44:07 +0530

    Create a method for interest calculation.

--- src/main/kotlin/com/ledgerco/lending/domain/Loan.kt
+++ src/main/kotlin/com/ledgerco/lending/domain/Loan.kt
@@ -4,4 +4,6 @@ class Loan(
     val principleAmount: Int,
     val periodInYears: Int,
     val rateOfInterest: Int
-)
+) {
+    fun interest() = (principleAmount * periodInYears * rateOfInterest) / 100.0
+}
